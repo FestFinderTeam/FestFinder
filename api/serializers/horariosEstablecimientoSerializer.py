7f57b4a84b08cb291e7fbdb0e581dc177a3430d8
Pablo <pablito.hans@hotmail.com>
2024-11-07 18:44:05 -0400

    prueba suicida

--- api/serializers/horariosEstablecimientoSerializer.py
+++ api/serializers/horariosEstablecimientoSerializer.py
@@ -5,7 +5,7 @@ from ..models import horariosEstablecimiento
 
 #prueba
 
-class HorariosEstablecimientoSerializer(serializers.ModelSerializer):
+class horariosEstablecimientoSerializer(serializers.ModelSerializer):
     class Meta:
         model = horariosEstablecimiento
         fields = ['establecimiento', 'dia_semana', 'inicio_atencion', 'fin_atencion']
