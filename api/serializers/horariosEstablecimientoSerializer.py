00e8d88743d7ff29a3002b9fc71164d8dfe31861
Pablo <pablito.hans@hotmail.com>
2024-11-07 18:47:44 -0400

    asd

--- api/serializers/horariosEstablecimientoSerializer.py
+++ api/serializers/horariosEstablecimientoSerializer.py
@@ -5,7 +5,7 @@ from ..models import horariosEstablecimiento
 
 #prueba
 
-class horariosEstablecimientoSerializer(serializers.ModelSerializer):
+class HorariosEstablecimientoSerializer(serializers.ModelSerializer):
     class Meta:
         model = horariosEstablecimiento
         fields = ['establecimiento', 'dia_semana', 'inicio_atencion', 'fin_atencion']
