8780de7b0bed585337f9a92973e6cf89e1bbc38c
Pablo <pablito.hans@hotmail.com>
2024-11-07 18:01:46 -0400

    xd

--- api/serializers/horariosEstablecimientoSerializer.py
+++ api/serializers/horariosEstablecimientoSerializer.py
@@ -1,7 +1,7 @@
 # api/serializers.py
 
 from rest_framework import serializers
-from ..models import  horariosEstablecimiento
+from ..models import horariosEstablecimiento
 
 class HorariosEstablecimientoSerializer(serializers.ModelSerializer):
     class Meta:
