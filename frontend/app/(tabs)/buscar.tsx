2b1a7b23d119c1998cb3e1507fc8ad3cb00820a7
JHUNIOR <202202131@est.umss.edu>
2024-11-09 19:46:48 -0400

    eliminacion de duplicados en el buscador

--- frontend/app/(tabs)/buscar.tsx
+++ frontend/app/(tabs)/buscar.tsx
@@ -24,7 +24,7 @@ const buscar = () => {
     };
     const fetchEventos = async () => {
         const res = await getEventosDelMes();
-        setEventos([...res, ...res, ...res]);
+        setEventos(res);
     };
     useEffect(() => {
         fetchEventos();
