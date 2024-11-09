8afb99e4d6d2e3fafd33d282db9a1f9656c4e8d0
Pablo <pablito.hans@hotmail.com>
2024-11-08 21:08:11 -0400

    serializador de eventos actualizado

--- api/serializers/EventoSerializer.py
+++ api/serializers/EventoSerializer.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+from .GeneroEventoSerializer import GeneroEventoSerializer
 from ..models import Evento
 
 class EventoSerializer(serializers.ModelSerializer):
+    id_genero_fk = GeneroEventoSerializer(read_only=True)  # Agregado para obtener datos completos del tipo
+
     class Meta:
         model = Evento
         fields = ['id_evento', 'nombre', 'banner', 'logo', 'fecha_inicio', 'id_establecimiento', 'fecha_final', 'horario_inicio', 'horario_fin', 'id_genero_fk']
