a70c0d8e2f1b7220c44b6a2ac2c8a154996105d0
Pablo <pablito.hans@hotmail.com>
2024-11-12 22:26:23 -0400

    serializador de usuarios mixto

--- api/serializers/UsuarioSerializer.py
+++ api/serializers/UsuarioSerializer.py
@@ -1,16 +1,23 @@
 from rest_framework import serializers
-from ..models import Usuario
+from ..models import Usuario, Imagen
 from django.contrib.auth.hashers import make_password
+from ..serializers import ImagenSerializer
 
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    # Usamos PrimaryKeyRelatedField para las solicitudes POST/PUT (registro)
+    imagen = serializers.PrimaryKeyRelatedField(queryset=Imagen.objects.all(), write_only=True)
+    
+    # Usamos el serializador completo solo para las solicitudes GET (lectura)
+    imagen_detail = ImagenSerializer(read_only=True, source='imagen')
+
     class Meta:
         model = Usuario
-        fields = '__all__'
-
+        fields = [
+            'id_usuario', 'nombre', 'email', 'telefono', 'p_field', 'g_id', 
+            'imagen', 'imagen_detail', 'duenio', 'establecimiento'
+        ]
 
     def validate_p_field(self, value):
         """Hash la contraseña antes de guardarla"""
         return make_password(value)
-    
-    
