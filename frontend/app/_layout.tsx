9fd7da7022bb904f35fc0add0d5a763e2b6a2223
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-11-14 18:51:04 -0400

    Wrap HomeLayout in SafeAreaView and add StatusBar for improved layout handling

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,21 +1,32 @@
 import { SessionProvider } from "@/hooks/ctx";
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
+import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
 import theme from "@/constants/theme";
 
 const HomeLayout = () => {
   return (
     <PaperProvider theme={theme}>
       <SessionProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="index" />
-          <Stack.Screen name="login" />
-          <Stack.Screen name="register" />
-        </Stack>
+        <SafeAreaView style={styles.container}>
+          <StatusBar barStyle="dark-content" backgroundColor={theme.colors.primary} />
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="index" />
+            <Stack.Screen name="login" />
+            <Stack.Screen name="register" />
+          </Stack>
+        </SafeAreaView>
       </SessionProvider>
     </PaperProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+});
 
 export default HomeLayout;
