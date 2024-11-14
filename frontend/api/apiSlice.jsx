829cebadff2b2afe7aa42987eadb78f061cc31ec
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-11-14 19:13:42 -0400

    Add Redux Toolkit integration with apiSlice and store configuration

---
+++ frontend/api/apiSlice.jsx
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import API_URL from "../constants/Url";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${API_URL}/api/`,
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({}),
+});
