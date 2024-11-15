e09b369fbbb8af8d1a629d6d05144e68dede4cf2
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-11-15 17:35:55 -0400

    Refactor HomeLayout component: simplify layout structure, remove unused styles, and enhance status bar handling

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,39 +1,31 @@
 import { SessionProvider } from "@/hooks/ctx";
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
-import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import { SafeAreaView, StatusBar } from "react-native";
 import theme from "@/constants/theme";
 import store from "@/store";
 import { Provider } from "react-redux";
 
 const HomeLayout = () => {
-  return (
-    <Provider store={store}>
-      <PaperProvider theme={theme}>
-        <SessionProvider>
-          <SafeAreaView style={styles.container}>
-            <StatusBar
-              backgroundColor={theme.colors.primary}
-              
-            />
-            <Stack screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="(tabs)" />
-              <Stack.Screen name="index" />
-              <Stack.Screen name="login" />
-              <Stack.Screen name="register" />
-            </Stack>
-          </SafeAreaView>
-        </SessionProvider>
-      </PaperProvider>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<PaperProvider theme={theme}>
+				<SessionProvider>
+					<StatusBar backgroundColor={theme.colors.primary} />
+					<SafeAreaView
+						style={{ flex: 1, paddingTop: StatusBar.currentHeight }}
+					>
+						<Stack screenOptions={{ headerShown: false }}>
+							<Stack.Screen name="(tabs)" />
+							<Stack.Screen name="index" />
+							<Stack.Screen name="login" />
+							<Stack.Screen name="register" />
+						</Stack>
+					</SafeAreaView>
+				</SessionProvider>
+			</PaperProvider>
+		</Provider>
+	);
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-//    paddingTop: StatusBar.currentHeight,
-  },
-});
 
 export default HomeLayout;
