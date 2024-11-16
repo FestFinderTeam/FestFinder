f73df453542ab5367a3c958e48d75111e3d01c8a
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-11-16 17:32:38 -0400

    Implement BottomSheet for event details in Mapa component, enhance loading state handling, and update layout structure in _layout.tsx

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -5,26 +5,29 @@ import { SafeAreaView, StatusBar } from "react-native";
 import theme from "@/constants/theme";
 import store from "@/store";
 import { Provider } from "react-redux";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const HomeLayout = () => {
 	return (
-		<Provider store={store}>
-			<PaperProvider theme={theme}>
-				<SessionProvider>
-					<StatusBar backgroundColor={theme.colors.primary} />
-					<SafeAreaView
-						style={{ flex: 1, paddingTop: StatusBar.currentHeight }}
-					>
-						<Stack screenOptions={{ headerShown: false }}>
-							<Stack.Screen name="(tabs)" />
-							<Stack.Screen name="index" />
-							<Stack.Screen name="login" />
-							<Stack.Screen name="register" />
-						</Stack>
-					</SafeAreaView>
-				</SessionProvider>
-			</PaperProvider>
-		</Provider>
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<Provider store={store}>
+				<PaperProvider theme={theme}>
+					<SessionProvider>
+						<StatusBar backgroundColor={theme.colors.primary} />
+						<SafeAreaView
+							style={{ flex: 1, paddingTop: StatusBar.currentHeight }}
+						>
+							<Stack screenOptions={{ headerShown: false }}>
+								<Stack.Screen name="(tabs)" />
+								<Stack.Screen name="index" />
+								<Stack.Screen name="login" />
+								<Stack.Screen name="register" />
+							</Stack>
+						</SafeAreaView>
+					</SessionProvider>
+				</PaperProvider>
+			</Provider>
+		</GestureHandlerRootView>
 	);
 };
 
