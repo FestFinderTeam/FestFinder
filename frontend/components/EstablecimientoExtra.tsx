bad11d54cc57f953f3a471d8f07171f57680cd4a
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-11-16 00:26:00 -0400

    Refactor EstablecimientoExtra and EventoExtra components: enhance image handling with default image fallback, improve optional chaining for properties, and ensure consistent score display for better user experience

--- frontend/components/EstablecimientoExtra.tsx
+++ frontend/components/EstablecimientoExtra.tsx
@@ -2,84 +2,85 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Link, type Href } from "expo-router";
 import React from "react";
 import { ImageBackground, Text, View, StyleSheet } from "react-native";
+const default_image = require("@/assets/images/default_image.png");
 
 export interface Place {
-    id: number;
-    nombre: string;
-    score: number;
-    views: number;
-    logo: any;
-    tipo_fk_detail?: any;
-    direccion?: string;
+	id: number;
+	nombre: string;
+	score: number;
+	views: number;
+	logo: any;
+	tipo_fk_detail?: any;
+	direccion?: string;
 }
 
 interface Props {
-    establecimiento: Place;
+	establecimiento: Place;
 }
 
 const Establecimiento = ({ establecimiento }: Props) => {
-    return (
-        <Link
-            href={("/places/" + establecimiento.id) as Href}
-            style={styles.link}
-        >
-            <View style={styles.container}>
-                <ImageBackground
-                    resizeMode="cover"
-                    source={{ uri: establecimiento.logo }}
-                    style={styles.imageBackground}
-                />
-                <View style={styles.details}>
-                    <Text style={styles.nombre}>{establecimiento.nombre}</Text>
-                    <Text style={styles.tipo}>
-                    {establecimiento.tipo_fk_detail.nombre_tipo}
-                    </Text>
-                    <Text style={styles.puntuacion}>
-                        <FontAwesome name="star" color="orange" />
-                        {` ${establecimiento.score} / 10`}
-                    </Text>
-                    <Text style={styles.puntuacion}>
-                        <FontAwesome name="circle" color="purple" />
-                        {" " + establecimiento.direccion}</Text>
-                </View>
-            </View>
-        </Link>
-    );
+	return (
+		<Link href={("/places/" + establecimiento.id) as Href} style={styles.link}>
+			<View style={styles.container}>
+				<ImageBackground
+					resizeMode="cover"
+					source={
+						establecimiento.logo ? { uri: establecimiento.logo } : default_image
+					}
+					style={styles.imageBackground}
+				/>
+				<View style={styles.details}>
+					<Text style={styles.nombre}>{establecimiento.nombre}</Text>
+					<Text style={styles.tipo}>
+						{establecimiento.tipo_fk_detail?.nombre_tipo}
+					</Text>
+					<Text style={styles.puntuacion}>
+						<FontAwesome name="star" color="orange" />
+						{` ${establecimiento.score ?? 0} / 10`}
+					</Text>
+					<Text style={styles.puntuacion}>
+						<FontAwesome name="circle" color="purple" />
+						{" " + establecimiento.direccion}
+					</Text>
+				</View>
+			</View>
+		</Link>
+	);
 };
 
 const styles = StyleSheet.create({
-    link: {
-        marginLeft: "3%",
-        marginBottom: 15,
-    },
-    container: {
-        flexDirection: "row", // Para alinear imagen y detalles en una fila
-        alignItems: "center",
-    },
-    imageBackground: {
-        width: 100,
-        height: 100,
-        borderRadius: 10,
-        overflow: "hidden",
-    },
-    details: {
-        marginLeft: 10,
-        flex: 1,
-    },
-    nombre: {
-        fontSize: 16,
-        fontWeight: "bold",
-        color: "black",
-    },
-    tipo: {
-        fontSize: 14,
-        color: "gray",
-    },
-    puntuacion: {
-        fontSize: 14,
-        color: "gray",
-        marginTop: 5,
-    },
+	link: {
+		marginLeft: "3%",
+		marginBottom: 15,
+	},
+	container: {
+		flexDirection: "row", // Para alinear imagen y detalles en una fila
+		alignItems: "center",
+	},
+	imageBackground: {
+		width: 100,
+		height: 100,
+		borderRadius: 10,
+		overflow: "hidden",
+	},
+	details: {
+		marginLeft: 10,
+		flex: 1,
+	},
+	nombre: {
+		fontSize: 16,
+		fontWeight: "bold",
+		color: "black",
+	},
+	tipo: {
+		fontSize: 14,
+		color: "gray",
+	},
+	puntuacion: {
+		fontSize: 14,
+		color: "gray",
+		marginTop: 5,
+	},
 });
 
 export default Establecimiento;
