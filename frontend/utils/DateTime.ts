6e3b4593b5c17ec5ea803e89b3f7866a4d0f4293
Jhunior Garcia <jhuniorremberto1@gmail.com>
2024-11-19 14:50:56 -0400

    fix: fechas

--- frontend/utils/DateTime.ts
+++ frontend/utils/DateTime.ts
@@ -32,8 +32,11 @@ export const showDate = (values: Date[], setValues: any, index: number) => {
 export const showSingleDate = (value: Date, setValue: any) => {
     DateTimePickerAndroid.open({
         value: value,
-        onChange: (event, selectedDate: Date | undefined) =>
-            selectedDate && setValue(selectedDate),
+        onChange: (event, selectedDate: Date | undefined) => {
+            console.log("fecha seleccionada", selectedDate);
+
+            selectedDate && setValue(selectedDate);
+        },
         mode: "date",
         is24Hour: true,
     });
@@ -65,7 +68,7 @@ export const dateToDDMMYYYY = (date: Date) =>
 
 export const dateToYYYYMMDD = (date: Date) => {
     const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDay();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
     return `${year}-${month}-${day}`;
 };
