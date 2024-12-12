5f7c843cbdcbbb29f81d7a9c5c2ea1a165906efe
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-12-12 18:45:38 -0400

    Enhance header navigation: add fallback routing when no history is available

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -5,36 +5,45 @@ import Styles from "../globalStyles/styles";
 import { router } from "expo-router";
 
 interface HeaderProps {
-	title: string;
+    title: string;
 }
 
 const Header = ({ title }: HeaderProps) => {
-	return (
-		<View style={Styles.headerView}>
-			<View style={styles.headerContent}>
-				<Pressable onPress={() => router.back()}>
-					<FontAwesome
-						name="arrow-left"
-						size={20}
-						color={"white"}
-						style={styles.icon}
-					/>
-				</Pressable>
-				<Text style={Styles.headerTitle}>{title}</Text>
-			</View>
-		</View>
-	);
+    return (
+        <View style={Styles.headerView}>
+            <View style={styles.headerContent}>
+                <Pressable
+                    onPress={() => {
+                        if (router.canGoBack()) {
+                            // Si hay historial previo, regresa
+                            router.back();
+                        } else {
+                            // Si no hay historial previo, redirige a una pantalla por defecto
+                            router.replace("/");
+                        }
+                    }}
+                >
+                    <FontAwesome
+                        name="arrow-left"
+                        size={20}
+                        color={"white"}
+                        style={styles.icon}
+                    />
+                </Pressable>
+                <Text style={Styles.headerTitle}>{title}</Text>
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-	headerContent: {
-		flexDirection: "row",
-		alignItems: "center",
-		
-	},
-	icon: {
-		marginRight: 10,
-	},
+    headerContent: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    icon: {
+        marginRight: 10,
+    },
 });
 
 export default Header;
